4db240aa7534e1dc5534d087bbf1a6cbf4a86aeb
Leliksey <captainfroody@gmail.com>
2021-01-11 16:35:49 +0200

    plugins & content

--- wp-content/themes/storefront-child-theme-master/functions.php
+++ wp-content/themes/storefront-child-theme-master/functions.php
@@ -23,4 +23,27 @@ function sf_child_theme_dequeue_style() {
  * Note: DO NOT! alter or remove the code above this text and only add your custom PHP functions below this text.
  */
 
+// add_filter( 'woocommerce_checkout_fields' , 'custom_override_checkout_fields' );
+  
+function custom_override_checkout_fields( $fields ) {
+  //unset($fields['billing']['billing_first_name']);// имя
+  //unset($fields['billing']['billing_last_name']);// фамилия
+  //unset($fields['billing']['billing_company']); // компания
+  //unset($fields['billing']['billing_address_1']);//
+  //unset($fields['billing']['billing_address_2']);//
+  //unset($fields['billing']['billing_city']);
+  //unset($fields['billing']['billing_postcode']);
+  //unset($fields['billing']['billing_country']);
+  //unset($fields['billing']['billing_state']);
+  //unset($fields['billing']['billing_phone']);
+  //unset($fields['order']['order_comments']);
+  unset($fields['billing']['billing_email']);
+  //unset($fields['account']['account_username']);
+  //unset($fields['account']['account_password']);
+  //unset($fields['account']['account_password-2']);
 
+ 
+  unset($fields['billing']['billing_company']);// компания
+  unset($fields['billing']['billing_postcode']);// индекс 
+    return $fields;
+}
